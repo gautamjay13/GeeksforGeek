d31e2a34b0382f8ade44e94af839c29bb0977d96
Jay Gautam <jaygautamlu13@gmail.com>
2026-06-23 10:45:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/While Loop/while-loop.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void utility(int x) {
+        // code here
+        while ( x>-1){
+            System.out.print(x +   " ");
+            x-- ;
+        }
+    }
+}
