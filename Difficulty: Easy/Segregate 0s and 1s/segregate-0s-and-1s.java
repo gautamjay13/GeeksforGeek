d8654c78811799d0f25060f3b576dc4df24f627a
Jay Gautam <jaygautamlu13@gmail.com>
2026-07-10 22:52:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.java
@@ -0,0 +1,19 @@
+class Solution {
+    void segregate0and1(int[] arr) {
+        // code here
+        int n = arr.length ; 
+        int i = 0 ; 
+        int j = n-1 ; 
+        while(i<j){
+            if(arr[i]==0) i++ ;
+            else if(arr[j]==1) j--;
+            else if (  arr[i]==1 && arr[j]==0){
+                arr[i]=0 ;
+                arr[j]=1 ;
+                i++ ;
+                 j-- ;
+            }
+        }
+        
+    }
+}
