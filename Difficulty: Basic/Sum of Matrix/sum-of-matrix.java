3ac8759368d5c1abad7416cba5943c3f81ca8e4f
Jay Gautam <jaygautamlu13@gmail.com>
2026-07-14 23:33:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Matrix/sum-of-matrix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sumOfMatrix(int[][] mat) {
+        // code here
+         int sum = 0;
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                sum += mat[i][j];
+            }
+        }
+
+        return sum;
+    }
+}
