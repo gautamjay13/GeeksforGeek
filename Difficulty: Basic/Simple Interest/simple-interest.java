307b3b6eab5690bead917940a4a76ab338646596
Jay Gautam <jaygautamlu13@gmail.com>
2026-08-08 23:18:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Simple Interest/simple-interest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int p = sc.nextInt();
+        int r = sc.nextInt();
+        int t = sc.nextInt();
+
+        // code here
+         double si = (p*r*t)/100.0 ;
+        System.out.printf("%.2f", si);
+    }
+}
