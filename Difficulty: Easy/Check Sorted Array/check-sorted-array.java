9356903d0178730bc6016d310438f2d8e9d03744
Jay Gautam <jaygautamlu13@gmail.com>
2026-08-23 12:58:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Sorted Array/check-sorted-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isSorted(int[] arr) {
+        // code here
+        for ( int i = 1 ; i < arr.length -1 ; i++ ){
+            if ( arr[i]>arr[i+1]) return false ;
+        }
+        return true ;
+    }
+}
